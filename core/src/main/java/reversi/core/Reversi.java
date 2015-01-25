d438894cd298ecff516c095f68d2784298d2422a
Michael Bayne <mdb@samskivert.com>
2015-01-24 16:06:45 -0800

    Created game model.

--- core/src/main/java/reversi/core/Reversi.java
+++ core/src/main/java/reversi/core/Reversi.java
@@ -1,11 +1,37 @@
 package reversi.core;
 
-import playn.core.Image;
-import playn.core.Platform;
-import playn.scene.ImageLayer;
-import playn.scene.SceneGame;
+import pythagoras.f.IDimension;
+import playn.core.*;
+import playn.scene.*;
+import react.RMap;
+import react.Value;
 
 public class Reversi extends SceneGame {
+
+  public static enum Piece { BLACK, WHITE }
+
+  public static class Coord {
+    public final int x, y;
+
+    public Coord (int x, int y) {
+      assert x >= 0 && y >= 0;
+      this.x = x;
+      this.y = y;
+    }
+
+    public boolean equals (Coord other) {
+      return other.x == x && other.y == y;
+    }
+    @Override public boolean equals (Object other) {
+      return (other instanceof Coord) && equals((Coord)other);
+    }
+    @Override public int hashCode () { return x ^ y; }
+    @Override public String toString () { return "+" + x + "+" + y; }
+  }
+
+  public final int boardSize = 8;
+  public final RMap<Coord,Piece> pieces = RMap.create();
+  public final Value<Piece> turn = Value.create(null);
 
   public Reversi (Platform plat) {
     super(plat, 33); // update our "simulation" 33ms (30 times per second)
